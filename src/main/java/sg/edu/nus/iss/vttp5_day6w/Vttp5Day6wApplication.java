219f7f3a8114b8c45aece47fc091a8a4988bc5a1
sherlynkek <sherlynkek00@gmail.com>
2024-12-09 15:32:54 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/vttp5_day6w/Vttp5Day6wApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5_day6w;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5Day6wApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5Day6wApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/vttp5_day6w/Vttp5Day6wApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5_day6w;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5Day6wApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
